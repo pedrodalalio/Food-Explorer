334cd541c74de2a282ea66f714f201d5d68a4a0d
pedrodalalio <pedrodalalio13@gmail.com>
2024-08-22 20:59:16 -0300

    Init

---
+++ backend/src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+
+const sqliteConnection = require('../database/sqlite');
+
+class UsersController{
+
+    async create(req, res){
+    }
+}
+
+module.exports = UsersController;
